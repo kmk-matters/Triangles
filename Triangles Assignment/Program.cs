274e6b865535776cce3d4f67b36dda0be0d43f07
kmk-matters <kmk.matters@gmail.com>
2020-06-08 18:52:29 +0530

    Add project files.

---
+++ Triangles Assignment/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Triangles_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float a = float.Parse(Console.ReadLine());
+
+            float b = float.Parse(Console.ReadLine());
+
+            float c = float.Parse(Console.ReadLine());
+
+
+            if (a + b > c)
+
+            {
+
+
+                if (a == b && b == c)
+
+
+                {
+
+
+                    Console.WriteLine("equilateral");
+
+
+                }
+
+
+                else if ((a * a + b * b - c * c) < 0.000001 || (b * b + c * c - a * a) < 0.000001 || (c * c + a * a - b * b) < 0.000001)
+
+
+                {
+
+
+                    Console.WriteLine("right-angled");
+
+
+                }
+
+
+                else if ((a == b) || (b == c) || (c == a))
+
+
+                {
+
+
+                    Console.WriteLine("isosceles");
+
+
+                }
+
+
+                else
+
+
+                {
+                    Console.WriteLine("notspecial");
+
+                }
+
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("invalid");
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
